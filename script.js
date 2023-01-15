1788340c40566f9e28a81ddb165c1f61f1566547
kevinleey <55768372+kevinleey@users.noreply.github.com>
2023-01-15 16:39:57 -0600

    Added animation logic to script.js, index.html, and style.css.

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
 var items = [2,8,7,1,3,5,6,4];
+var container = document.getElementById("array");
+var desc = document.getElementById("desc");
 
-// swaps items of indices l and r
-function swap(items, l, r) {
-    var temp = items[l];
-    items[l] = items[r];
-    items[r] = temp;
+function generateArray() {
+    for (var i = 0; i < items.length; i++) {
+
+        // Create div element
+        var array_ele = document.createElement("div");
+        
+        // Adding class 'block' to div
+        array_ele.classList.add("block");
+        
+        // Add height to blocks
+        array_ele.style.height = `${items[i] * 3}px`;
+
+        // Create label for block
+        var array_ele_label = document.createElement("label");
+        array_ele_label.classList.add("block_id");
+        array_ele_label.innerHTML = items[i];
+
+        // Append label to block, then append block to array
+        array_ele.appendChild(array_ele_label);
+        container.appendChild(array_ele);
+    }
 }
 
 /*
@@ -12,30 +30,91 @@ function swap(items, l, r) {
     such that the left half has values < x and the latter half has values > x.
     Returns the new index of pivot.
 */
-function partition(items, p, r) {
-    var pivot = items[r];
+async function partitionEl(p, r) {
+    var blocks = document.querySelectorAll(".block");
+
+    var pivot = Number(blocks[r].childNodes[0].innerHTML);
+
+    // Set pivot block color to red
+    blocks[r].style.backgroundColor = 'red';
+    
+    // Update description text
+    desc.innerHTML = `Pivot point chosen: ${pivot}`;
+    
     var i = p - 1;
+
+    // wait 2 seconds
+    await new Promise((resolve) =>
+                setTimeout(() => {
+                    resolve();
+                }, 2000)
+            );
+
     for (let j = p; j < r; j++) {
-        if (items[j] < pivot) {
+        if (Number(blocks[j].childNodes[0].innerHTML) < pivot) {
             i++;
-            swap(items, i, j);
+            
+            // Swapping mechanism
+            var temp1 = blocks[i].style.height;
+            var temp2 = blocks[i].childNodes[0].innerHTML;
+            blocks[i].style.height = blocks[j].style.height;
+            blocks[j].style.height = temp1;
+            blocks[i].childNodes[0].innerHTML = blocks[j].childNodes[0].innerHTML;
+            blocks[j].childNodes[0].innerHTML = temp2;
+
+            // Changing colors accordingly
+            blocks[i].style.backgroundColor = 'orange';
+            blocks[j].style.backgroundColor = 'green';
+            if (i == j) blocks[i].style.backgroundColor = 'orange';
+
+            blocks = document.querySelectorAll(".block");
+        } else {
+            blocks[j].style.backgroundColor = 'green';
         }
+
+        await new Promise((resolve) =>
+                    setTimeout(() => {
+                    resolve();
+                }, 700)
+            );
     }
     i++;
-    swap(items, i, r);
+
+    // Swapping mechanism (pivot's swap)
+    var temp1 = blocks[i].style.height;
+    var temp2 = blocks[i].childNodes[0].innerHTML;
+    blocks[i].style.height = blocks[r].style.height;
+    blocks[r].style.height = temp1;
+    blocks[i].childNodes[0].innerHTML = blocks[r].childNodes[0].innerHTML;
+    blocks[r].childNodes[0].innerHTML = temp2;
+
+    // Changing colors accordingly
+    blocks[r].style.backgroundColor = 'green';
+    blocks[i].style.backgroundColor = 'red';
+
+    await new Promise((resolve) =>
+        setTimeout(() => {
+        resolve();
+        }, 2100)
+    );
+
     return i;
 }
 
-function quicksort(items, p, r) {
+async function quicksortEl(p, r) {
     if (p < r) {
-        var q = partition(items, p, r);
-        quicksort(items, p, q-1);
-        quicksort(items, q+1, r);
+        var q = await partitionEl(p, r);
+
+        // Reset blocks' colors
+        blocks = document.querySelectorAll(".block");
+        for (var i = 0; i < blocks.length; i++) {
+            blocks[i].style.backgroundColor = "#6b5b95";
+        }
+
+        await quicksortEl(p, q-1);
+        await quicksortEl(q+1, r);
     }
 }
 
-console.log(items);
-
-quicksort(items, 0, items.length-1);
-
-console.log(items);
+generateArray();
+quicksortEl(0, items.length-1);
